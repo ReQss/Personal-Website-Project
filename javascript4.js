a344bed7c381ef09244dc613ad9f1afa8dcddc78
ReQss <46198811+ReQss@users.noreply.github.com>
2021-10-24 03:19:30 +0200

    Update javascript4.js

--- javascript4.js
+++ javascript4.js
@@ -174,3 +174,50 @@ function CloseAlert(){
 	document.getElementById("bioinfo").style.display = 'none';
 	
 }
+
+//RED - 256,0,0 GREEN 0,256,0 BLUE 0,0,256
+var r=256;
+var g=0;
+var b=0;
+var ink=0;
+function rgb(){
+  console.log(r);
+  var folder = document.getElementById("rgb").style.color =`rgb(${r},${g},${b})`;
+  var folder = document.getElementById("napis").style.color =`rgb(${r},${g},${b})`;
+  /*
+if(ink==0){
+r+=1; 
+g-=1;
+}
+if(r>=256)
+ink=1;
+if(ink==1){
+r-=1;
+g+=1;
+if(r<=0)
+ink=0;
+}*/
+if(ink==0){
+  r-=1; 
+  g+=1;
+  if(g==256)
+  ink=1;
+  }
+  if(ink==1){
+    g-=1;
+    b+=1;
+    if(b==256)
+    ink=2;
+  }
+  if(ink==2){
+    b-=1;
+    r+=1;
+    if(r==256)
+    ink=0;
+  }
+
+
+
+console.log(r,g);
+}
+var myInterval = setInterval(rgb, 5);
